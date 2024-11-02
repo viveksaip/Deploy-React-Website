814f2f80f37c3a1918f7d4e4991cb35508d48b08
Vivek Sai <viveks@Viveks-MacBook-Air.local>
2024-11-02 19:25:31 +0530

    chan

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+
         <p>
             VS webiste rules the space.
         </p>
 
-      </header>
-    </div>
+   
   );
 }
 
